b2a145c509586a00794fe761521edea15257ce51
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2023-08-14 19:17:10 -0300

    Adicionado comentário para falar quais são as atividades.

--- Aula2/main.py
+++ Aula2/main.py
@@ -18,7 +18,7 @@ def upper(nome):
         "upper": nome.upper()
     }
 
-@app.get("/ehpar/{numero}")
+@app.get("/ehpar/{numero}") // Atividade 1
 def ehpar(numero: int):
     if numero % 2 == 0:
         return {
@@ -28,7 +28,7 @@ def ehpar(numero: int):
         "result": False
     }
 
-@app.get("/horarios/{cidade}")
+@app.get("/horarios/{cidade}") // Atividade 2
 def horarios(cidade):
     if cidade == "Tokyo":
         return {
@@ -46,7 +46,7 @@ def horarios(cidade):
             "Hora": datetime.now() + timedelta(hours=4)
         }
 
-@app.get("/dia")
+@app.get("/dia") // Atividade 3
 def dia():
     dia = datetime.now().weekday()
     if dia == 0:
